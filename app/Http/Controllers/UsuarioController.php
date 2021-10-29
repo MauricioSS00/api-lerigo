8edabc9ad262defed6dc284749c70b3049b9ba26
Mauricio da Silva <maurcioss83@gmail.com>
2021-10-29 06:26:39 -0300

    Novas Rotas

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\DB;
@@ -11,13 +12,45 @@ class UsuarioController extends BaseController
 {
     use AuthorizesRequests;
 
-    public function listarEventos()
+    public function listarUsuarios(): array
     {
-        return DB::select("SELECT * FROM evento");
+        return DB::select("SELECT * FROM users");
     }
 
-    public function salvarEvento(Request $request)
+    public function listarUsuario(int $codUsuario)
     {
-        return $request;
+        $usuario = DB::select("SELECT * FROM users WHERE id = $codUsuario");
+        return is_array($usuario) ? $usuario[0] : [];
+    }
+
+    public function salvarUsuario(Request $request): bool
+    {
+        $request = $request[0];
+        $endereco = $request["endereco"][0];
+        $tipoPessoa = $request["tipoP"] == "cpf" ? "F" : "J";
+        $dtNasc = date("d/m/Y", strtotime($request["dtNasc"]));
+        $senha = bcrypt($request["user_password"]);
+        $SQL = <<<SQL
+INSERT INTO
+    users
+(
+    nome, nome_social, email, password, estrangeiro, tipo_pessoa, documento, genero, rg_pass, orgao_rg_pass,
+    uf_rg_pass, data_nascimento, telefone, celular, whatsapp, cep, uf, cidade, logradouro,
+    bairro, numero, complemento
+)
+VALUES
+(
+    '{$request["user_nome_civ"]}', '{$request["user_soc_nome"]}', '{$request["user_email"]}', '$senha', {$request["estrangeiro"]},
+    '$tipoPessoa', '{$request["doc"]}', '{$request["genero"]}', '{$request["user_pic_id"]}', '{$request["user_id_doc"]}', '{$request["user_id_state"]}',
+    '$dtNasc', '{$request["fone"]}', '{$request["celular"]}', '{$request["user_whats"]}', '{$endereco["cep"]}', '{$endereco["uf"]}', '{$endereco["localidade"]}',
+    '{$endereco["logradouro"]}', '{$endereco["bairro"]}', '{$endereco["numero"]}', '{$endereco["complemento"]}'
+)
+SQL;
+        try {
+            DB::select($SQL);
+            return true;
+        } catch (Exception $e) {
+            throw new Exception("Ocorreu um erro");
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/eventos', [UsuarioController::class, "listarEventos"]);
-Route::post('/eventos', [UsuarioController::class, "salvarEvento"]);
+Route::post('/login', [AuthController::class, "login"]);
+Route::get('/usuarios', [UsuarioController::class, "listarUsuarios"]);
+Route::post('/usuario', [UsuarioController::class, "salvarUsuario"]);
+Route::get('/usuario/{codUsuario}', [UsuarioController::class, "listarUsuario"]);
